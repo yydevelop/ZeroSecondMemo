e10f4a6f4fcf9517b29c5c5b6f9a6488290d0b34
yydevelop <yasudadevelop@gmail.com>
2024-10-20 11:19:08 +0900

    空の入力フォーム

--- app/src/main/java/com/example/zerosecondmemo/MainActivity.kt
+++ app/src/main/java/com/example/zerosecondmemo/MainActivity.kt
@@ -9,8 +9,8 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge() // Edge to Edge モードの有効化
+        setContentView(R.layout.activity_main) // レイアウトの設定
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
